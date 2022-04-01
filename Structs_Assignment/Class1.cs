8a29f5a59ffcaa0765c803cf81d95ca55edc09ab
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-03-31 17:02:51 -0700

    adding anther small c sharp program
    more c sharp code

---
+++ Structs_Assignment/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Assignment
+{
+    public struct Class1
+    {
+        public decimal number { get; set; }
+
+
+    }
+}
---
+++ Structs_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Structs_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 classes = new Class1();
+            classes.number = 100.25m;
+
+            Console.WriteLine(classes.number);
+        }
+    }
+}
