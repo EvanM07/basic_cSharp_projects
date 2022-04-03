0203c1c011e0f8263f184f8d1ddb8047a6dbf884
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-04-03 10:07:08 -0700

    File I/O Assignment
    For logging purposes only !

---
+++ File_IO_Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please input a words or numbers ");
+
+            string userInput = "";
+
+            userInput = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Laptop\Documents\GitHub\basic_cSharp_projects\File_IO_Assignment\log\log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+
+
+        }
+    }
+}
