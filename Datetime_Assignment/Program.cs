98d8714c5029650703bdcbac29da559e23cfa69c
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-04-03 18:09:16 -0700

    DateTime
    Making small c sharp program that has date and time

---
+++ Datetime_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Datetime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please write a line");
+
+            Console.WriteLine("Please input a number from 1 to 12");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The time will be " + DateTime.Now.AddHours(userInput));
+
+
+
+        }
+    }
+}
