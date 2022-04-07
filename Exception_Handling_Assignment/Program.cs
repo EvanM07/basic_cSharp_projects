de705b6c7ee2e9ff2ffd999776b01ab080958870
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2022-04-06 17:43:00 -0700

    Exception handling
    small c sharp program

---
+++ Exception_Handling_Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Exception_Handling_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in your age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is " + Age);
+
+            if (Age <= 0) 
+            {
+            try
+            {
+
+
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("Number can't be under zero!!!!!!!!!!!!!!");
+
+
+
+            }
+            }
+
+
+         
+
+
+        }
+    }
+}
